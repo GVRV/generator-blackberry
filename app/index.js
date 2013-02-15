1221c111a914091a49fc90e9df337ad51a6d5e64
Gaurav Dadhania <gaurav@dadhania.in>
2013-02-16 01:28:17 +1100

    fix yeoman path for index.js

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 'use strict';
 var util = require('util');
 var path = require('path');
-var yeoman = require('yeoman-generator');
+var yeoman = require('../../../../');
 
 
 module.exports = AppGenerator;
